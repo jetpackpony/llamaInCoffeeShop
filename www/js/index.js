c688e525cf92251c481ec524f9fa6207087c5e4d
jetpackpony <jetpackpony@gmail.com>
2017-04-13 20:17:07 +0300

    Remove generated cordova code

--- www/js/index.js
+++ www/js/index.js
@@ -1,9 +1,10 @@
-import getTest from './getTest';
-
 var app = {
     initialize: function() {
-      document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-      document.getElementById('test').innerText = getTest();
+      document.addEventListener(
+        'deviceready',
+        this.onDeviceReady.bind(this),
+        false
+      );
     },
 
     onDeviceReady: function() {
