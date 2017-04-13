3a54395c436f526085f439d0e6bea07a3ac4f41e
jetpackpony <jetpackpony@gmail.com>
2017-04-13 13:30:51 +0300

    Add ES6 support and webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 var path = require('path');
 
 module.exports = {
-  entry: './www/js/index.js',
+  devtool: 'eval-source-map',
+  entry: ['babel-polyfill', './www/js/index.js'],
   output: {
     path: path.join(__dirname, 'www'),
     filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "www"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+            plugins: [require('babel-plugin-transform-object-rest-spread')]
+          }
+        }
+      }
+    ]
   }
 };
