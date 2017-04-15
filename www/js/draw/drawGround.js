88ea8c85ddf316dee35126740c1812a9676ff37f
jetpackpony <jetpackpony@gmail.com>
2017-04-15 18:54:24 +0300

    Add the ground so it is visible

--- www/js/draw/drawGround.js
+++ www/js/draw/drawGround.js
@@ -3,14 +3,14 @@ import unpackCanvas from './unpackCanvas';
 export default function drawGround(canvas, state) {
   const { width, height, ctx } = unpackCanvas(canvas);
   const tileWidth = state.ground.tileWidth;
-  const tileHeight = 50;
+  const tileHeight = 150;
 
   ctx.save();
 
   ctx.translate(state.ground.position, height - tileHeight);
 
-  ctx.fillStyle = 'rgb(200, 0, 0)';
-  ctx.fillRect(0, 0, width + tileWidth, 50);
+  ctx.fillStyle = ctx.createPattern(state.statics.images.groundTile.imgObject, 'repeat');
+  ctx.fillRect(0, 0, width + tileWidth, 150);
 
   ctx.restore();
 };
