610cf346d6319c7cc828a0ce5627ee8b265af473
jetpackpony <jetpackpony@gmail.com>
2017-04-15 18:54:40 +0300

    Refactor load images

--- www/js/loadImages.js
+++ www/js/loadImages.js
@@ -1,22 +1,26 @@
+const collect = (res, img) => ({
+  ...res,
+  [img.key]: img.value
+});
+
+const loadImage = (title, url) => {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.addEventListener('load', () => {
+      resolve({
+        key: title,
+        value: { imgObject: img }
+      });
+    }, false);
+    img.src = url;
+  });
+};
+
 export default function loadImages(imgUrls) {
   let promises = Object.keys(imgUrls).map((title) => {
-    return new Promise((resolve, reject) => {
-      const img = new Image();
-      img.addEventListener('load', function() {
-        console.log('loaded: ', imgUrls[title], img);
-        resolve({
-          key: title,
-          value: {
-            imgObject: img
-          }
-        });
-      }, false);
-      img.src = imgUrls[title];
-    });
+    return loadImage(title, imgUrls[title]);
   });
   return Promise
     .all(promises)
-    .then((imgs) => {
-      return imgs.reduce((res, img) => res[img.key] = img.value, {});
-    });
+    .then((imgs) => imgs.reduce(collect, {}));
 };
