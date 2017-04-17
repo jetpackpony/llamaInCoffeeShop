53b8e73fdb43f2430dc79d8c832dbbe587ab77cb
jetpackpony <jetpackpony@gmail.com>
2017-04-17 22:48:30 +0300

    Add konva and react

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['env'],
-            plugins: [require('babel-plugin-transform-object-rest-spread')]
+            plugins: [require('babel-plugin-transform-object-rest-spread'), require('babel-plugin-transform-react-jsx')]
           }
         }
       }
