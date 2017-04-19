7373cf113f85be3ab24161d08fe88dfc6aa12e40
jetpackpony <jetpackpony@gmail.com>
2017-04-19 12:19:04 +0300

    Update player to use react-redux

--- www/js/components/Player.js
+++ www/js/components/Player.js
@@ -1,32 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 import { Image } from 'react-konva';
 
-export default class Player extends Component {
-  constructor(...args) {
-    super(...args);
-    this.state = {
-      image: null,
-      position: { x: 10, y: 10 }
-    };
-  }
-  componentDidMount() {
-    const image = new window.Image();
-    image.onload = () => {
-      const height = 70;
-      const width = image.width * height / image.height;
-      this.setState({ image, width, height });
-    };
-    image.src = 'img/llama.png';
-  }
-  render() {
-    return (
-      <Image
-        x={this.state.position.x}
-        y={this.state.position.y}
-        image={this.state.image}
-        width={this.state.width}
-        height={this.state.height}
-      />
-    );
-  }
+const mapStateToProps = (state) => {
+  const player = state.world.objects.find((obj) => obj.id === 'player');
+  const image = state.assets.images.player.imgObject;
+  const height = state.world.player.height;
+  const width = image.width * height / image.height;
+
+  // invert the y coordinate
+  const y = state.world.height - player.body.position.y;
+
+  return {
+    x: player.body.position.x,
+    y: y,
+    image,
+    height,
+    width
+  };
 };
+
+export default connect(mapStateToProps)(Image);
