5259964add158112867d69f6917ce241667f01ab
jetpackpony <jetpackpony@gmail.com>
2017-04-29 18:31:16 +0300

    Use ramda.evolve for tickGround

--- www/js/reducer/tickGround.js
+++ www/js/reducer/tickGround.js
@@ -1,51 +1,47 @@
 import R from 'ramda';
-import * as Utils from  '../utils';
+import { clipValue } from  '../utils';
 import { calcVelocity, calcPosition, updateBody } from '../physics';
 import {
   MIN_GROUND_SPEED, MAX_GROUND_SPEED, GROUND_TILE_WIDTH
 } from '../constants';
 
-const clipValue = Utils.clipValue(MIN_GROUND_SPEED, MAX_GROUND_SPEED);
-const clipXVelocity = (oldVel) => clipValue(Math.abs(oldVel)) * -1;
 const clipXPosition = (oldPos) => oldPos % GROUND_TILE_WIDTH;
+const clipXVelocity = (oldVel) => (
+  clipValue(MIN_GROUND_SPEED, MAX_GROUND_SPEED, Math.abs(oldVel)) * -1
+);
 const calcXAcceleration = R.curry((energy, oldAcc) => (
   (energy > 90) ? -50 : 50
 ));
-const calcXDiff = R.curry((oldPos, newPos, oldDiff) => {
+const calcXDiff = R.curry((oldPos, newPos) => {
   const xDiff = Math.abs(newPos) - Math.abs(oldPos);
   return (xDiff <= 0) ? GROUND_TILE_WIDTH - xDiff : xDiff;
 });
 
-const xPosLens = R.lensPath(['position', 'x']);
-const xVelLens = R.lensPath(['velocity', 'x']);
-const xAccLens = R.lensPath(['acceleration', 'x']);
-const xDiffLens = R.lensProp('xDiffSinceLastTick');
-
-const modifyProperty = R.curry((lens, updater, body) => {
-  return R.set(lens, updater(R.view(lens, body)), body);
-});
-
 const modifyXDiff = R.curry((prevX, body) => (
-  modifyProperty(xDiffLens, calcXDiff(prevX, body.position.x), body)
+  R.set(R.lensProp('xDiffSinceLastTick'), calcXDiff(prevX, body.position.x), body)
 ));
 
 export default (world) => {
+  const body = world.ground.body;
+  const transformations = {
+    position: { x: clipXPosition },
+    velocity: { x: clipXVelocity },
+    acceleration: { x: calcXAcceleration(world.score.energy) },
+    lastTick: () => world.timestamp
+  };
   const updateGroundBody = R.compose(
-    modifyXDiff(world.ground.body.position.x),
-    modifyProperty(xAccLens, calcXAcceleration(world.score.energy)),
-    modifyProperty(xVelLens, clipXVelocity),
-    modifyProperty(xPosLens, clipXPosition),
+    modifyXDiff(body.position.x),
+    R.evolve(transformations),
     updateBody
   );
-  const timeDiff = (world.timestamp - world.ground.body.lastTick) / 1000;
+  const timeDiff = (world.timestamp - body.lastTick) / 1000;
 
   return {
     ...world,
     ground: {
       ...world.ground,
       body: {
-        ...updateGroundBody(world.ground.body, timeDiff),
-        lastTick: world.timestamp
+        ...updateGroundBody(body, timeDiff),
       }
     }
   };
