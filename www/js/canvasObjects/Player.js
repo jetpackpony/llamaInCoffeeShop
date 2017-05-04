5c2883feb5875c3b46d8a0e0b43039b6ad35abde
jetpackpony <jetpackpony@gmail.com>
2017-05-04 18:02:33 +0300

    Add player animated sprite

--- www/js/canvasObjects/Player.js
+++ www/js/canvasObjects/Player.js
@@ -1,16 +1,27 @@
 import * as PIXI from 'pixi.js';
+import R from 'ramda';
 
 export default function createOrUpdatePlayer(player, state) {
   const data = getPlayerData(state);
   return updatePlayer(player || createPlayer(data), data);
 };
 
-function createPlayer({ width, height }) {
+function createPlayer(data) {
+  let player = new PIXI.Container();
+
+  var anim = new PIXI.extras.AnimatedSprite(data.images);
+  anim.width = data.width;
+  anim.height = data.height;
+  anim.animationSpeed = 0.5;
+  anim.play();
+
   var rect = new PIXI.Graphics();
   rect.beginFill(0x9932CC);
-  rect.drawRect(0, 0, width, height);
+  rect.drawRect(0, 0, data.width, data.height);
   rect.endFill();
-  return rect;
+
+  player.addChild(rect, anim);
+  return player;
 }
 
 function updatePlayer(player, data) {
@@ -25,10 +36,12 @@ function getPlayerData(state) {
   const { width, height } = player;
   const x = player.body.position.x;
   const y = state.world.height - player.body.position.y - height;
+  const imgNames = R.times((i) => (`llama0${i + 1}.png`), 6);
   return {
     x: x + offset,
     y: y + offset,
     width: width - offset*2,
     height: height - offset*2,
+    images: Object.values(R.pick(imgNames, state.assets.images))
   }
 }
