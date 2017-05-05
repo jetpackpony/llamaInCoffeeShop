982dadbe9b96a0717f9cd3486198fae50bcc5f34
jetpackpony <jetpackpony@gmail.com>
2017-05-05 20:31:26 +0300

    Add jumping sprites to llama

--- www/js/canvasObjects/Player.js
+++ www/js/canvasObjects/Player.js
@@ -22,21 +22,71 @@ function createPlayer(data) {
     polygon.endFill();
     player.addChild(polygon);
   }
+  var running = new PIXI.extras.AnimatedSprite(data.images.running);
+  running.id = 'running';
+  running.width = data.width;
+  running.height = data.height;
+  running.animationSpeed = 0.5;
+  running.play();
+  player.addChild(running);
 
-  var anim = new PIXI.extras.AnimatedSprite(data.images);
-  anim.width = data.width;
-  anim.height = data.height;
-  anim.animationSpeed = 0.5;
-  anim.play();
-  player.addChild(anim);
+  var jumping = new PIXI.extras.AnimatedSprite(data.images.jumping);
+  jumping.id = 'jumping';
+  jumping.width = data.width;
+  jumping.height = data.height;
+  jumping.animationSpeed = 0.1;
+  jumping.loop = false;
+  jumping.visible = false;
+  player.addChild(jumping);
 
   return player;
 }
 
 function updatePlayer(player, data) {
+  if (data.jump) {
+    startJump(player);
+  }
+  if (data.gameState !== 'playing') {
+    stopAnimation(player);
+  } else {
+    startAnimationIfStopped(player);
+  }
   player.x = data.x;
   player.y = data.y;
   return player;
+}
+
+function startJump(player) {
+  const running = player.children.find((o) => o.id === 'running');
+  const jumping = player.children.find((o) => o.id === 'jumping');
+
+  running.visible = false;
+  jumping.visible = true;
+  jumping.onComplete = () => startRunning(player);
+  jumping.play();
+}
+
+function startRunning(player) {
+  const running = player.children.find((o) => o.id === 'running');
+  const jumping = player.children.find((o) => o.id === 'jumping');
+
+  running.visible = true;
+  jumping.visible = false;
+}
+
+function stopAnimation(player) {
+  const running = player.children.find((o) => o.id === 'running');
+  const jumping = player.children.find((o) => o.id === 'jumping');
+
+  running.stop();
+  jumping.stop();
+}
+
+function startAnimationIfStopped(player) {
+  const running = player.children.find((o) => o.id === 'running');
+  const jumping = player.children.find((o) => o.id === 'jumping');
+
+  if (!running.playing) running.play();
 }
 
 function getPlayerData(state) {
@@ -44,13 +94,23 @@ function getPlayerData(state) {
   const { width, height } = player;
   const x = player.body.position.x;
   const y = state.world.height - player.body.position.y - height;
-  const imgNames = R.times((i) => (`llama0${i + 1}.png`), 6);
+  const runImgNames = R.times((i) => (`llama0${i + 1}.png`), 6);
+  const jumpImgNames = R.times((i) => (`llama-jump0${i + 1}.png`), 6);
   return {
     x: x,
     y: y,
     width: width,
     height: height,
-    images: Object.values(R.pick(imgNames, state.assets.images)),
-    collisionBounds: player.collisionBounds
+    images: {
+      running: Object.values(R.pick(runImgNames, state.assets.images)),
+      jumping: Object.values(R.pick(jumpImgNames, state.assets.images)),
+    },
+    collisionBounds: player.collisionBounds,
+    jump: isPlayerJumping(player.body.position.y, state),
+    gameState: state.world.gameState
   }
+}
+
+function isPlayerJumping(playerY, state) {
+  return playerY > state.world.groundHeight + 5;
 }
